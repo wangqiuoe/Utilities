d343bc34e3f6bee1c72c3cb520e5f06d0eec961b
wangqi <wangqiuoe@126.com>
2022-03-30 12:48:32 -0400

    update

--- CUTEst2Matlab/solveCUTEstProblemNew.m
+++ CUTEst2Matlab/solveCUTEstProblemNew.m
@@ -1,4 +1,4 @@
-function info = solveCUTEstProblem(problem,algorithm_config, user_dir, algorithm_perf_sub_dir)
+function info = solveCUTEstProblemNew(problem,algorithm_config, user_dir, algorithm_perf_sub_dir)
 
 if startsWith(pwd,'/Users/wangqi/')
     % local conf
@@ -37,22 +37,22 @@ addpath(solver_path)
 
 % Create problem objects and parameters
 P = cutest_setup();
+x0 = P.x;
 
-% common parameters
+
+
+%% function handles and parameters for local optimizer solver
 hands.f_hand = @cutest_obj;
 hands.g_hand = @cutest_grad;
 hands.H_hand = @cutest_hess;
 hands.Hv_hand = @cutest_hprod;
-x0 = P.x;
-hands.l = x0 - rand(size(x0));  % rand: uniform 0-1
-hands.u = x0 + rand(size(x0));
-
+hands.l = x0 - 0.1;  % rand: uniform 0-1
+hands.u = x0 + 0.1;
 params.maxtime    = 90*60;   % max 10 minutes for each instance
 params.maxiter    = 100;
 params.printlevel = 1;
 params.tol        = 1e-4;
 params.problem    = problem;
-
 function setparams(fieldname, default_value)
     % is param is given
     if isfield(params_given, fieldname)
@@ -66,17 +66,45 @@ function setparams(fieldname, default_value)
         params.(fieldname) = default_value;
     end
 end
-
 % Trust Region parameters
 %setparams('step_type', 'NewtonCG'); %'NewtonCG';%'CauchyStep'; %'More-Sorensen';
 %setparams('algorithm', 'exact');
 %setparams('xi', 1);
-
 params.outfileID = fopen(sprintf('%s/%s/log_%s_%s.out', user_dir, algorithm_perf_sub_dir,algorithm_full_name,problem),'w');
-% Optimize
-%[x, info] = S(hands, x0, params);
+
+%% Optimize
+function [f_val,g_val] = cutest_func(x)
+    f_val = cutest_obj(x);
+    g_val = cutest_grad(x);
+end
 try
+    %% compute unconstrained sol
+    options = optimoptions('fminunc','Algorithm','trust-region','SpecifyObjectiveGradient',true);
+    fun = @cutest_func;
+    [x_unc, f_unc] = fminunc(fun,x0,options);
+    
+    %% for stochastic IPM
+    if startsWith(algorithm, 'StochasticIPM')
+        
+        % compute determinied sol
+        ipm_params = params;
+        ipm_params.maxiter = 1e4;
+        ipm_params.sub_printlevel=0;
+        ipm = IPM(x0,hands,ipm_params);
+        [x_det, info_det] = ipm.solve();
+        
+        % generate yi
+        M = 100;
+        y_std = 0.1;
+        y = x_det + y_std*randn(length(x_det), M);
+        hands.y = y;
+        params.L = info_det.L;
+        params.maxiter=1000; % max stochastic gradient evaluations
+        params.tol = 1e-3;
+    end
     S = eval(sprintf('%s(x0,hands,params)', algorithm));
+    
+    %% 
     [x, info] = S.solve();
 catch ME
     if strcmp(ME.identifier, 'MATLAB:nomem')
@@ -104,16 +132,16 @@ norm_r    = info.norm_r;
 iter      = info.iter;
 n         = length(x0);
 
-% Save solution
+%% Save solution
 fileID = fopen(sprintf('%s/%s/measure_%s.txt', user_dir, algorithm_perf_sub_dir,algorithm_full_name), 'a');
 % TODO: correct the order of metrics in plotPerformanceProfile
 fprintf(fileID, '%s\t%g\t%g\t%g\t%.5f\t%g\t%g\t%g\t%.5f\t%.5f\t%s\n', problem, n, status,iter, time,g_evals,f_evals, Hv_evals, f, norm_r, outcome);
 fclose(fileID);
 % ------------------------------------------------------------------
 
-% Delete
+%% Delete
 cutest_terminate;
 
-% Move back
+%% Move back
 cd(user_dir);
 end
