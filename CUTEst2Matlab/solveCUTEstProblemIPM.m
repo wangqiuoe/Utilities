9db0489561a506f81ac0a3a02c24949f4c893957
wangqiuoe <wangqiuoe@126.com>
2022-08-28 17:32:26 -0400

    update

--- CUTEst2Matlab/solveCUTEstProblemIPM.m
+++ CUTEst2Matlab/solveCUTEstProblemIPM.m
@@ -26,6 +26,8 @@ for i=1:length(fields)
     if strcmp(kv_key,'solver')
         algorithm = kv_value;
         algorithm_full_name = algorithm;
+    elseif strcmp(kv_key,'opt_mse')
+        opt_mse = str2num(kv_value);
     else
         params_given.(kv_key) = kv_value;
         algorithm_full_name = sprintf('%s-%s', algorithm_full_name, kv_value);
@@ -39,8 +41,6 @@ addpath(solver_path)
 P = cutest_setup();
 x0 = P.x;
 
-
-
 %% function handles and parameters for local optimizer solver
 hands.f_hand = @cutest_obj;
 hands.g_hand = @cutest_grad;
@@ -49,12 +49,38 @@ hands.Hv_hand = @cutest_hprod;
 hands.l = P.bl;
 hands.u =  P.bu; %x0 + 10;% for debug P.bu;
 
-%% general parameters
+% Move x0 if it is not interior
+for idx=1:length(x0)
+
+    % Revise u(i) and l(i) if they are equal, l = x0 - 1 and u = x0 + 1 
+    if hands.u(idx) == hands.l(idx)
+        hands.l(idx) = x0(idx) - 1;
+        hands.u(idx) = x0(idx) + 1;
+    end
+
+    % Revise x0(i) if x0(i) is not interior between l(i) and u(i)
+    if (x0(idx) >= hands.u(idx)) || (x0(idx) <= hands.l(idx))
+        if (hands.u(idx) == 1e20) && (hands.l(idx) == -1e20)
+            x0(idx) = 0;
+            problem = sprintf('%s_unb', problem);
+        elseif (hands.u(idx) == 1e20)
+            x0(idx) = hands.l(idx) + 1;
+        elseif (hands.l(idx) == -1e20)
+            x0(idx) = hands.u(idx) - 1;
+        else
+            x0(idx) = (hands.u(idx) + hands.l(idx)) / 2;
+        end
+    end
+end
+
+% general parameters
 params.maxtime    = 60*60;
 params.maxiter    = 100;
 params.printlevel = 1;
 params.tol        = 1e-4;
 params.problem    = problem;
+params.outfile_name = sprintf('%s/%s/log_%s_%s.out', user_dir, algorithm_perf_sub_dir,algorithm_full_name,problem);
+
 function setparams(fieldname, default_value)
     % is param is given
     if isfield(params_given, fieldname)
@@ -68,105 +94,101 @@ function setparams(fieldname, default_value)
         params.(fieldname) = default_value;
     end
 end
-params.outfile_name = sprintf('%s/%s/log_%s_%s.out', user_dir, algorithm_perf_sub_dir,algorithm_full_name,problem);
-if isfile(params.outfile_name)
-    delete(params.outfile_name)
-end
 
-%% Optimize
-function [f_val,g_val] = cutest_func(x)
-    f_val = cutest_obj(x);
-    g_val = cutest_grad(x);
-end
+
 try
-    %% Move x0 if it is not interior
-    for idx=1:length(x0)
-        % Revise u(i) and l(i) if they are equal 
-        if hands.u(idx) == hands.l(idx)
 
-            % if l and u are zero, l = -1 and u = 1 
-            if hands.u(idx) == 0
-                hands.u(idx) =  1;
-                hands.l(idx) = -1;
-            else
-                hands.u(idx) = hands.u(idx) + 0.1 * abs(hands.u(idx));
-                hands.l(idx) = hands.l(idx) - 0.1 * abs(hands.l(idx));
-            end
-        end
-
-        % Revise x0(i) if x0(i) is not interior between l(i) and u(i)
-        if (x0(idx) >= hands.u(idx)) || (x0(idx) <= hands.l(idx))
-            if (hands.u(idx) == 1e20) && (hands.l(idx) == -1e20)
-                x0(idx) = 0;
-            elseif (hands.u(idx) == 1e20)
-                x0(idx) = hands.l(idx) + 1;
-            elseif (hands.l(idx) == -1e20)
-                x0(idx) = hands.u(idx) - 1;
-            else
-                x0(idx) = (hands.u(idx) + hands.l(idx)) / 2;
-            end
-        end
-    end
-
-    %% check if x0 is an iterior point
+    % check if x0 is an iterior point
     assert (( sum(x0 < hands.u) == length(x0) ) && ( sum(x0 > hands.l) == length(x0) ), 'x0 is not an interior point')
 
-    %%% compute unconstrained sol
-    %options = optimoptions('fminunc','Algorithm','trust-region','SpecifyObjectiveGradient',true);
-    %fun = @cutest_func;
-    %[x_unc, f_unc] = fminunc(fun,x0,options);
-    %fprintf('unconstrained norm_x: %.4f, f: %.4f', norm(x_unc), f_unc)
-    
-    %% for stochastic IPM
-
-    % check if deterministic solution exists
     ipm_solution_filename=sprintf('%s/IPMSolution/%s.mat', user_dir, problem);
-    assert (isfile(ipm_solution_filename), 'no deterministic solution exists')
-    
-    % load deterministic sol
-    DetSol=load(ipm_solution_filename);
-    x_det=DetSol.x;
-    L_det=DetSol.L;
 
-    % generate yi
-    M = 100;
-    y_std = 0.1;
-    rng(28);
-    y = x_det + y_std*randn(length(x_det), M);
-    fprintf('y(1)=%11.6e', y(1));
-    if startsWith(algorithm, 'StochasticIPM')
-        hands.y = y;
-        params.L = L_det;
-        params.maxiter=1000; % max stochastic gradient evaluations
-        params.tol = 1e-4;
-    elseif startsWith(algorithm, 'IPM')
-        hands.f_hand = @(x) cutest_obj(x) + 1/M * sum( sum((x - y).^2,1) );
-        hands.g_hand = @(x) cutest_grad(x) + 2/M * sum(x - y, 2);
-    end
-    S = eval(sprintf('%s(x0,hands,params)', algorithm));
+    % solve f_cutest by IPM solver
+    if ~opt_mse
+
+        ipm = IPM(x0,hands,params);
+        [x, info] = ipm.solve();
+        x_star = x;
+        info_star = info;
+        if info.status==1
+            save(ipm_solution_filename,'x_star','info_star')
+        end
+
+    % solve (f_cutest - fi)^2 MSE problem
+    elseif opt_mse
+
+        % get x_star by loading from file
+        assert( isfile(ipm_solution_filename), 'no f_cutest solution exists')
+        ipm_sol=load(ipm_solution_filename);
+        x_star=ipm_sol.x_star;
+        info_star=ipm_sol.info_star;
     
-    %% 
-    [x, info] = S.solve();
+        % generate m xi
+        m  = 1000;
+        sigma = 0.001*eye(length(x_star));
+        rng(0);
+        xis = mvnrnd(x_star', sigma, m)';      % n by m
+        fis = zeros(m,1);
+        for i = 1:m
+            fis(i) = cutest_obj(xis(:,i));
+        end
+
+        % construct mse objective and gradient function
+        hands.m = m;
+        hands.f_hand = @(x) 1/m * sum((cutest_obj(x) - fis).^2);
+        hands.g_hand = @(x) 2/m .* cutest_grad(x) * ones(1, m) * (cutest_obj(x) - fis);
+        hands.sf_hand = @(x, idx) 1/length(idx) * sum((cutest_obj(x) - fis(idx)).^2);
+        hands.sg_hand = @(x, idx) 2/length(idx) .* cutest_grad(x) * ones(1, length(idx)) * (cutest_obj(x) - fis(idx));
+
+        % construct params
+        clear params;
+        params.problem          = sprintf('%s_mse', problem);
+        params.printlevel       = 1;
+        params.outfile_name     = sprintf('%s/%s/log_%s_%s.out', user_dir, algorithm_perf_sub_dir,algorithm_full_name,problem);
+        if strcmp(algorithm, 'IPM')
+            params.maxtime      = 60*60;   
+            params.maxiter      = 100;
+            params.tol          = 1e-4;
+        elseif strcmp(algorithm, 'StochasticIPM')
+            % params that is to be tuned
+            params.max_subiter  = setparams('max_subiter', 1);
+            params.strategy     = setparams('strategy', 'proj_ext');
+            % fixed params
+            params.maxtime      = 60*60;   
+            params.maxiter      = 10000;
+            params.Lf           = abs(info_star.Lf * sum(2 * info_star.f_max - fis));
+            params.alpha_init   = 1e-3;
+        end
+
+        % call solver
+        S = eval(sprintf('%s(x0,hands,params)', algorithm));
+        [x, info] = S.solve();
+
+    end % if optimize MSE problem
+
 catch ME
     if strcmp(ME.identifier, 'MATLAB:nomem')
         info.status=-3;
     elseif strcmp(ME.message, 'x0 is not an interior point')
         info.status=-4;
-    elseif strcmp(ME.message, 'no deterministic solution exists')
+    elseif strcmp(ME.message, 'no f_cutest solution exists')
         info.status=-5;
     else
         info.status=-1;
     end
-    info.time   = -1;
     info.outcome = ME.message;
-    info.f       = -1;
+    info.time    = -1;
     info.iter    = -1; 
-end
+    info.f       = nan;
+    info.norm_r  = nan;
+end % try
+
 status    = info.status;
 outcome   = info.outcome;
 time      = info.time/60;
-f         = info.f;
 iter      = info.iter;
+f         = info.f;
+norm_r    = info.norm_r;
 n         = length(x0);
 
 %% Save solution
@@ -174,20 +196,13 @@ measure_filename = sprintf('%s/%s/measure_%s.txt', user_dir, algorithm_perf_sub_
 % print header of measure
 if ~isfile(measure_filename)
     fileID = fopen(measure_filename, 'w');
-    fprintf(fileID, '%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'problem', 'n', 'status','iter', 'time', 'f', 'outcome');
+    fprintf(fileID, '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'problem', 'n', 'status','iter', 'time', 'f', 'norm_r', 'outcome');
 end
+% print metrics of instance
 fileID = fopen(measure_filename, 'a');
 % TODO: correct the order of metrics in plotPerformanceProfile
-fprintf(fileID, '%s\t%g\t%g\t%g\t%.5f\t%.5f\t%s\n', problem, n, status,iter, time,f, outcome);
+fprintf(fileID, '%s\t%g\t%g\t%g\t%.5f\t%11.4e\t%11.4e\t%s\n', problem, n, status,iter, time,f, norm_r, outcome);
 fclose(fileID);
-
-%%% Save x and info.L if startsWith(algorithm, 'IPM')
-%if strcmp(algorithm, 'IPM') && info.status==1
-%    L = info.L;
-%    solution_filename=sprintf('%s/IPMSolution/%s.mat', user_dir, problem);
-%    save(solution_filename,'x','L')
-%end
-% ------------------------------------------------------------------
 
 %% Delete
 cutest_terminate;
